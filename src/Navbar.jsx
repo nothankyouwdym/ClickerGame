1cd661f19ac586ce3fe02683d0bea915a310992c
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-05 15:01:01 -0500

    5 files modified, 1 added, 2 deleted

---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+
+class Navbar extends Component{
+  render(){
+    return(
+      <section>
+        <nav>
+          <div id="Title">
+            <h3>Nav</h3>
+          </div>
+          <div id="Links">
+    
+          </div>
+        </nav>
+      </section>
+    )
+  }
+}
+
+
+export default Navbar
