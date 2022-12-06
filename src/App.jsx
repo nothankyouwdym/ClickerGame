746407195ee6f4a781b5e1eeca78a343d582e209
nothankyouwdym <105501075+nothankyouwdym@users.noreply.github.com>
2022-12-05 20:00:35 -0500

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React, { Component } from "react"
+import React, { Component, useState } from "react"
 import { Outlet } from "react-router-dom"
+import Boomer from "./Boomer";
 import Navbar from "./Navbar"
 
 
 
-class App extends Component{
-  render(){
+function App(){
+    const [count,setCount] = useState(0);
+
+    const handleClick = () =>{
+      setCount(count+1);
+    }
+
+    document.addEventListener("keydown", (event) =>{
+      if(event.key === "Space"){
+        setCount(count+1);
+      }
+    })
     return(
       <section>
         <div className="Nav">
@@ -13,11 +24,16 @@ class App extends Component{
         </div>
         <div id="Title">
           <h1>Spacebar Clicker</h1>
+          <p>*click on the button to use spacebar*</p>
+        </div>
+
+        <div id="Input">
+          <h1>{count}</h1>
+          <button onClick={handleClick}>Spacebar</button>
         </div>
         <Outlet />
       </section>
     )
-  }
 }
 
 
